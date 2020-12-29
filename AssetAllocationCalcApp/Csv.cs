96d9d0e761cceb0dc423230e2fb3db2b7f7f1ea3
PlasticBucket1944 <Orangina2739@gmail.com>
2020-12-29 22:41:20 +0900

    CSV制御用クラス作成

---
+++ AssetAllocationCalcApp/Csv.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssetAllocationCalcApp
+{
+    /// <summary>
+    /// CSV制御用クラス
+    /// </summary>
+    class Csv
+    {
+        public Csv()
+        { 
+        }
+    }
+}
