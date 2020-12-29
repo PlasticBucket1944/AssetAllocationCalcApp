cac52f786d0f720f49b32b72e6cc94d5587e3f0c
PlasticBucket1944 <Orangina2739@gmail.com>
2020-12-29 22:26:26 +0900

    グリッドビューの名前変更

--- AssetAllocationCalcApp/InputForm.Designer.cs
+++ AssetAllocationCalcApp/InputForm.Designer.cs
@@ -31,68 +31,74 @@
             this.btnImport = new System.Windows.Forms.Button();
             this.textImportPath = new System.Windows.Forms.TextBox();
             this.lblImportInfo = new System.Windows.Forms.Label();
-            this.gridImportList = new System.Windows.Forms.DataGridView();
+            this.gridViewImportList = new System.Windows.Forms.DataGridView();
             this.btnImportComplete = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.gridImportList)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridViewImportList)).BeginInit();
             this.SuspendLayout();
             // 
             // btnImport
             // 
-            this.btnImport.Location = new System.Drawing.Point(12, 384);
+            this.btnImport.Location = new System.Drawing.Point(9, 307);
+            this.btnImport.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnImport.Name = "btnImport";
-            this.btnImport.Size = new System.Drawing.Size(79, 46);
+            this.btnImport.Size = new System.Drawing.Size(59, 37);
             this.btnImport.TabIndex = 0;
             this.btnImport.Text = "取り込み";
             this.btnImport.UseVisualStyleBackColor = true;
             // 
             // textImportPath
             // 
-            this.textImportPath.Location = new System.Drawing.Point(102, 405);
+            this.textImportPath.Location = new System.Drawing.Point(76, 324);
+            this.textImportPath.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textImportPath.Name = "textImportPath";
-            this.textImportPath.Size = new System.Drawing.Size(595, 22);
+            this.textImportPath.Size = new System.Drawing.Size(447, 19);
             this.textImportPath.TabIndex = 1;
             // 
             // lblImportInfo
             // 
-            this.lblImportInfo.Location = new System.Drawing.Point(102, 384);
+            this.lblImportInfo.Location = new System.Drawing.Point(76, 307);
+            this.lblImportInfo.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblImportInfo.Name = "lblImportInfo";
-            this.lblImportInfo.Size = new System.Drawing.Size(595, 18);
+            this.lblImportInfo.Size = new System.Drawing.Size(446, 14);
             this.lblImportInfo.TabIndex = 2;
             this.lblImportInfo.Text = "ここに取り込み結果が表示されますここに取り込み結果が表示されますここに取り込み結果が表示されますここに取り込み結果が表示されますここに取り込み結果が表示されます";
             // 
-            // gridImportList
+            // gridViewImportList
             // 
-            this.gridImportList.AllowUserToOrderColumns = true;
-            this.gridImportList.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.gridImportList.Location = new System.Drawing.Point(12, 12);
-            this.gridImportList.Name = "gridImportList";
-            this.gridImportList.RowHeadersWidth = 51;
-            this.gridImportList.RowTemplate.Height = 24;
-            this.gridImportList.Size = new System.Drawing.Size(776, 356);
-            this.gridImportList.TabIndex = 3;
+            this.gridViewImportList.AllowUserToOrderColumns = true;
+            this.gridViewImportList.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.gridViewImportList.Location = new System.Drawing.Point(9, 10);
+            this.gridViewImportList.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.gridViewImportList.Name = "gridViewImportList";
+            this.gridViewImportList.RowHeadersWidth = 51;
+            this.gridViewImportList.RowTemplate.Height = 24;
+            this.gridViewImportList.Size = new System.Drawing.Size(582, 285);
+            this.gridViewImportList.TabIndex = 3;
             // 
             // btnImportComplete
             // 
-            this.btnImportComplete.Location = new System.Drawing.Point(709, 384);
+            this.btnImportComplete.Location = new System.Drawing.Point(532, 307);
+            this.btnImportComplete.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnImportComplete.Name = "btnImportComplete";
-            this.btnImportComplete.Size = new System.Drawing.Size(79, 46);
+            this.btnImportComplete.Size = new System.Drawing.Size(59, 37);
             this.btnImportComplete.TabIndex = 4;
             this.btnImportComplete.Text = "取り込み確定";
             this.btnImportComplete.UseVisualStyleBackColor = true;
             // 
             // InputForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(600, 360);
             this.Controls.Add(this.btnImportComplete);
-            this.Controls.Add(this.gridImportList);
+            this.Controls.Add(this.gridViewImportList);
             this.Controls.Add(this.lblImportInfo);
             this.Controls.Add(this.textImportPath);
             this.Controls.Add(this.btnImport);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "InputForm";
             this.Text = "CSV取り込み画面";
-            ((System.ComponentModel.ISupportInitialize)(this.gridImportList)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridViewImportList)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -103,7 +109,7 @@
         private System.Windows.Forms.Button btnImport;
         private System.Windows.Forms.TextBox textImportPath;
         private System.Windows.Forms.Label lblImportInfo;
-        private System.Windows.Forms.DataGridView gridImportList;
+        private System.Windows.Forms.DataGridView gridViewImportList;
         private System.Windows.Forms.Button btnImportComplete;
     }
 }
