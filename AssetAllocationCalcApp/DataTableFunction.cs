b898f199eda9abbf5d8268bb4833390bc0f06fd9
PlasticBucket1944 <Orangina2739@gmail.com>
2021-01-17 23:38:16 +0900

    データテーブル用ファンクションクラス新規作成

---
+++ AssetAllocationCalcApp/DataTableFunction.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssetAllocationCalcApp
+{
+    /// <summary>
+    /// データテーブル用ファンクションクラス
+    /// </summary>
+    class DataTableFunction
+    {
+    }
+}
